93deed0ea153d00d3757ef8ebdb106ede58b363f
taras <pakito_home@ukr.net>
2023-05-31 14:41:30 +0300

    init project

---
+++ app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.webviewtest
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.webviewtest.viewmodel.MainViewModel
+import kotlinx.coroutines.launch
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btPrev: Button
+    private lateinit var btNext: Button
+    private lateinit var viewContent: WebView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+        configureWebView()
+        val viewModel: MainViewModel by viewModels()
+        handleViewEvents(viewModel)
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { state ->
+                    when (state) {
+                        is MainViewModel.UIState.Loading -> {
+                            viewModel.loadLink(MainViewModel.UIEvent.NONE)
+                        }
+                        is MainViewModel.UIState.Result -> {
+                            viewContent.loadUrl(state.url);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun configureWebView() {
+        viewContent.settings.javaScriptEnabled = true
+    }
+
+    private fun handleViewEvents(viewModel: MainViewModel) {
+        btPrev.setOnClickListener {
+            viewModel.loadLink(MainViewModel.UIEvent.PREV)
+        }
+        btNext.setOnClickListener {
+            viewModel.loadLink(MainViewModel.UIEvent.NEXT)
+        }
+        viewContent.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                handleContentVisibility(false)
+            }
+            override fun onPageFinished(view: WebView, url: String) {
+                handleContentVisibility(true)
+            }
+        }
+    }
+
+    private fun handleContentVisibility(isContentVisible: Boolean = false) {
+        if (isContentVisible) {
+            btPrev.visibility = View.VISIBLE
+            btPrev.visibility = View.VISIBLE
+        } else {
+            btPrev.visibility = View.GONE
+            btPrev.visibility = View.GONE
+        }
+    }
+
+
+    private fun initViews() {
+        viewContent = findViewById(R.id.web_view_content)
+        btPrev = findViewById(R.id.bt_prev)
+        btPrev = findViewById(R.id.bt_next)
+    }
+}
---
+++ app/src/main/java/com/example/webviewtest/viewmodel/MainViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.webviewtest.viewmodel
+
+import android.app.Application
+import androidx.annotation.StringRes
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _uiState = MutableStateFlow<UIState>(UIState.Loading)
+    val uiState: StateFlow<UIState> = _uiState.asStateFlow()
+    private val linkArray = listOf(
+        "https://www.amazon.com/-/de/dp/B07N3RFXRL/ref=pd_ci_mcx_mh_ci_mcx_mr_mp_m_0?pd_rd_w=C8JmI&content-id=amzn1.sym.07889413-fea5-4a13-b06c-d39edf4aa03e&pf_rd_p=07889413-fea5-4a13-b06c-d39edf4aa03e&pf_rd_r=49PVZJ7PQETKW6WJ8R3N&pd_rd_wg=I909t&pd_rd_r=74e6ce3d-f18f-4e84-b9a5-b9073478d5e0&pd_rd_i=B07N3RFXRL&th=1",
+        "https://www.youtube.com/watch?v=LbWjVNjlpjA",
+        "https://www.spiegel.de/"
+    )
+    private var linkCounter = 0
+
+    fun loadLink(event: UIEvent) {
+        findLinkId(event)
+        _uiState.value = UIState.Result(linkArray[linkCounter])
+    }
+
+    private fun findLinkId(event: UIEvent) {
+        when (event) {
+            UIEvent.PREV -> {
+                linkCounter = if (linkCounter == 0) {
+                    2
+                } else {
+                    linkCounter.dec()
+                }
+            }
+            UIEvent.NEXT -> {
+                linkCounter = if (linkCounter == 2) {
+                    0
+                } else {
+                    linkCounter.inc()
+                }
+            }
+            else -> {
+                linkCounter = 0
+            }
+        }
+    }
+
+    sealed class UIState {
+        object Loading : UIState()
+        data class Result(val url: String) : UIState()
+    }
+
+    enum class UIEvent {
+        NONE, PREV, NEXT
+    }
+}
